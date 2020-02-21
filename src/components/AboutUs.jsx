75233a053780194191282bbf57413764039931f7
Nobu <rockmusic2090@gmail.com>
2020-02-20 22:34:23 -0800

    Added Button component and style for Button component

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Button from "./Button";
+
+const AboutUs = () => {
+  return (
+    <div className="aboutUs">
+      <h3>About Us</h3>
+      <h2>We make a modern and </h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum, atque.
+        Minus accusantium doloremque quos sit accusamus repellat officiis in
+        facere voluptatem molestiae! Quis est mollitia doloremque voluptatem
+        facere, hic ab.
+      </p>
+      <Button text="Explore Us" />
+    </div>
+  );
+};
+
+export default AboutUs;
