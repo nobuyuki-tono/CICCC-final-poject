abcf2430b9ca70b73a048d8745f52b1593d9ff9c
Tatsuya167 <clark.taka16@gmail.com>
2020-02-22 09:53:05 +0900

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-
 import "./App.css";
+
+import Header from "./views/Header/header.jsx"
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/views/Header/header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import "./header.scss";
+
+const Header = () => {
+
+    return (
+        <>  
+            <section id="header_part">
+                <div className="header_container">
+                    <div className="row align_items">
+                            <div className="offset_left">
+                                <div className="header_text">
+                                    <div className="header_text_inner">
+                                        <h5>We are digital agency</h5>
+                                        <h1>Digital and innovative idea</h1>
+                                        <a href="#" className="btn_ex_work">Explore work</a>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Header;
