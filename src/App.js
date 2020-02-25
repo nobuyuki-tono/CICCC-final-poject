56c97745d68f91ff26754356a65a318601588355
Nobu <rockmusic2090@gmail.com>
2020-02-25 13:53:11 -0800

    Created Team component and added style to it

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import ContactForm from "./components/ContactForm/ContactForm";
 
 import "./App.css";
 import AboutUs from "./components/AboutUs";
+import Team from "./components/Team";
 
 function App() {
   return (
     <div className="App">
       <AboutUs />
+      <Team />
       <ContactForm />
     </div>
   );
---
+++ src/components/Team.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "../styles/Team.scss";
+
+const Team = () => {
+  return (
+    <div className="team">
+      <h3>Our team</h3>
+
+      <p>Nobuyuki Tono - Fullstack web developer</p>
+      <p>Shin Daimon - Fullstack web developer</p>
+      <p>Tatsuya Takarabe - Fullstack web developer</p>
+    </div>
+  );
+};
+
+export default Team;
