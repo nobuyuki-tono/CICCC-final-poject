f8e947536227ed188fca0c5169763bbdf1bbc1f1
40 daimon <tvwmdaimon40@gmail.com>
2020-02-26 10:17:58 -0800

    add Footer component and fontawesome package

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
